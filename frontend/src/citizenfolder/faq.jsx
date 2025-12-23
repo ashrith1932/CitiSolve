9da0cdd2eda7eea1337dc70eecf677181c754753
ashrith1932 <tummalaashrithreddy2391@gmail.com>
2025-12-23 12:29:21 +0530

    Fix casing in import statement for CSS module

--- frontend/src/citizenfolder/faq.jsx
+++ frontend/src/citizenfolder/faq.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./Complaintstyle.module.css";
+import styles from "./complaintstyle.module.css";
 
 const FAQ = () => {
   const navigate = useNavigate();
